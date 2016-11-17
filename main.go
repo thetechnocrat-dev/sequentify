c00a1bc4e765bee1a671a42694d20c2240439135
McMenemy <mr_mcmenemy@yahoo.com>
2016-11-17 14:47:35 -0800

    add json example route

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/McMenemy/aligner"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"runtime"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	parrot := aligner.Tester("howdy")
+func jsonHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	type Example struct {
+		Pairs []string
+	}
+	e := Example{
+		Pairs: []string{"gg", "g_", "__", "tt"},
+	}
+	res, err := json.Marshal(e)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	parrot := aligner.Tester("hiswdy")
 	fmt.Fprintf(w, "%s, this is sequentify, i am running on %s with an %s cpu", parrot, runtime.GOOS, runtime.GOARCH)
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	router := httprouter.New()
+	router.GET("/", indexHandler)
+	router.GET("/json", jsonHandler)
+	http.ListenAndServe(":8080", router)
 }
