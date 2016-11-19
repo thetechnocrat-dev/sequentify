bbaea6d3bb6156a2600be00bd1c21101b9b283fd
McMenemy <mr_mcmenemy@yahoo.com>
2016-11-18 23:09:22 -0800

    implement cors

--- main.go
+++ main.go
@@ -9,17 +9,64 @@ import (
 	"runtime"
 )
 
+func corsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
 func jsonHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	type Example struct {
 		Pairs []string
 	}
 	e := Example{
-		Pairs: []string{"gg", "g_", "__", "tt"},
+		Pairs: []string{"gg", "g_", "aa", "tt"},
 	}
 	res, err := json.Marshal(e)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	type Post struct {
+		Test []string
+	}
+	decoder := json.NewDecoder(r.Body)
+	var pS Post
+	err := decoder.Decode(&pS)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pS.Test[0])
+}
+
+func alignHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	type SeqPair struct {
+		SeqA string
+		SeqB string
+	}
+	decoder := json.NewDecoder(r.Body)
+	var seqs SeqPair
+	err := decoder.Decode(&seqs)
+	if err != nil {
+		panic(err)
+	}
+	alignment := aligner.Align(seqs.SeqA, seqs.SeqB)
+	res, err := json.Marshal(alignment)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
 }
@@ -32,6 +79,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func main() {
 	router := httprouter.New()
 	router.GET("/", indexHandler)
-	router.GET("/json", jsonHandler)
+	router.POST("/json", jsonHandler)
+	router.OPTIONS("/*any", corsHandler)
+	router.POST("/testpost", postHandler)
+	router.POST("/align", alignHandler)
 	http.ListenAndServe(":8080", router)
 }
