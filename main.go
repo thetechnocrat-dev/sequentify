15c545ed9231be9ddef3cda257d006e6f81f7428
McMenemy <mr_mcmenemy@yahoo.com>
2016-11-29 11:00:25 -0800

    remove unnecessary routes

--- main.go
+++ main.go
@@ -6,45 +6,12 @@ import (
 	"github.com/McMenemy/aligner"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"runtime"
 )
 
 func corsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://sequentify.com")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
-
-func jsonHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	type Example struct {
-		Pairs []string
-	}
-	e := Example{
-		Pairs: []string{"gg", "g_", "aa", "tt"},
-	}
-	res, err := json.Marshal(e)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-	w.Header().Set("Access-Control-Allow-Origin", "http://sequentify.com")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	type Post struct {
-		Test []string
-	}
-	decoder := json.NewDecoder(r.Body)
-	var pS Post
-	err := decoder.Decode(&pS)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(pS.Test[0])
 }
 
 func alignHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -72,16 +39,13 @@ func alignHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	parrot := aligner.Tester("hiswdy")
-	fmt.Fprintf(w, "%s, this is sequentify, i am running on %s with an %s cpu", parrot, runtime.GOOS, runtime.GOARCH)
+	fmt.Fprintf(w, "This is sequentify's resftful API")
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", indexHandler)
-	router.POST("/json", jsonHandler)
 	router.OPTIONS("/*any", corsHandler)
-	router.POST("/testpost", postHandler)
 	router.POST("/align", alignHandler)
 	http.ListenAndServe(":8080", router)
 }
